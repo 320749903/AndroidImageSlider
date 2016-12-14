90735d7847ed935af5dfef472d0d103750f3101a
think <think@localhost.localdomain>
2016-12-14 17:37:10 +0800

    use fresco to load image.

--- library/src/main/java/com/daimajia/slider/library/SliderTypes/BaseSliderView.java
+++ library/src/main/java/com/daimajia/slider/library/SliderTypes/BaseSliderView.java
@@ -1,14 +1,25 @@
 package com.daimajia.slider.library.SliderTypes;
 
 import android.content.Context;
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.daimajia.slider.library.R;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.RequestCreator;
+import com.facebook.common.util.UriUtil;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.image.ImageInfo;
+//import com.squareup.picasso.Callback;
+//import com.squareup.picasso.Picasso;
+//import com.squareup.picasso.RequestCreator;
 
 import java.io.File;
 
@@ -47,7 +58,8 @@ public abstract class BaseSliderView {
 
     private String mDescription;
 
-    private Picasso mPicasso;
+//    private Picasso mPicasso;
+    DraweeController mImageLoader;
 
     /**
      * Scale type of the image.
@@ -188,7 +200,7 @@ public abstract class BaseSliderView {
      * @param v the whole view
      * @param targetImageView where to place image
      */
-    protected void bindEventAndShow(final View v, ImageView targetImageView){
+    protected void bindEventAndShow(final View v, final SimpleDraweeView targetImageView){
         final BaseSliderView me = this;
 
         v.setOnClickListener(new View.OnClickListener() {
@@ -207,61 +219,113 @@ public abstract class BaseSliderView {
             mLoadListener.onStart(me);
         }
 
-        Picasso p = (mPicasso != null) ? mPicasso : Picasso.with(mContext);
-        RequestCreator rq = null;
+
+        Uri uri = null;
         if(mUrl!=null){
-            rq = p.load(mUrl);
+            uri = android.net.Uri.parse(mUrl);
         }else if(mFile != null){
-            rq = p.load(mFile);
+            uri = android.net.Uri.fromFile(mFile);
         }else if(mRes != 0){
-            rq = p.load(mRes);
+            uri = new Uri.Builder()
+                    .scheme(UriUtil.LOCAL_RESOURCE_SCHEME) // "res"
+                    .path(String.valueOf(mRes))
+                    .build();
         }else{
             return;
         }
 
-        if(rq == null){
-            return;
-        }
-
-        if(getEmpty() != 0){
-            rq.placeholder(getEmpty());
-        }
-
-        if(getError() != 0){
-            rq.error(getError());
-        }
-
-        switch (mScaleType){
-            case Fit:
-                rq.fit();
-                break;
-            case CenterCrop:
-                rq.fit().centerCrop();
-                break;
-            case CenterInside:
-                rq.fit().centerInside();
-                break;
-        }
-
-        rq.into(targetImageView,new Callback() {
+        ControllerListener listener = new BaseControllerListener<ImageInfo>() {
             @Override
-            public void onSuccess() {
-                if(v.findViewById(R.id.loading_bar) != null){
-                    v.findViewById(R.id.loading_bar).setVisibility(View.INVISIBLE);
-                }
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                updateViewSizeRatio(targetImageView, imageInfo);
             }
 
             @Override
-            public void onError() {
-                if(mLoadListener != null){
-                    mLoadListener.onEnd(false,me);
-                }
-                if(v.findViewById(R.id.loading_bar) != null){
-                    v.findViewById(R.id.loading_bar).setVisibility(View.INVISIBLE);
-                }
+            public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable animatable) {
+                updateViewSizeRatio(targetImageView, imageInfo);
             }
-        });
+        };
+
+        DraweeController loader;
+//        if (mImageLoader != null) {
+//            loader = mImageLoader;
+//            loader.
+//        }
+//        else {
+            loader = Fresco.newDraweeControllerBuilder()
+                    .setControllerListener(listener)
+                    .setUri(uri)
+                    // other setters
+                    .build();
+//            Picasso.with(mContext);
+//        }
+
+        targetImageView.setController(loader);
+
+
+//        if(rq == null){
+//            return;
+//        }
+//
+//        if(getEmpty() != 0){
+//            rq.placeholder(getEmpty());
+//        }
+//
+//        if(getError() != 0){
+//            rq.error(getError());
+//        }
+//
+//        switch (mScaleType){
+//            case Fit:
+//                rq.fit();
+//                break;
+//            case CenterCrop:
+//                rq.fit().centerCrop();
+//                break;
+//            case CenterInside:
+//                rq.fit().centerInside();
+//                break;
+//        }
+
+//        rq.into(targetImageView,new Callback() {
+//            @Override
+//            public void onSuccess() {
+//                if(v.findViewById(R.id.loading_bar) != null){
+//                    v.findViewById(R.id.loading_bar).setVisibility(View.INVISIBLE);
+//                }
+//            }
+//
+//            @Override
+//            public void onError() {
+//                if(mLoadListener != null){
+//                    mLoadListener.onEnd(false,me);
+//                }
+//                if(v.findViewById(R.id.loading_bar) != null){
+//                    v.findViewById(R.id.loading_bar).setVisibility(View.INVISIBLE);
+//                }
+//            }
+//        });
    }
+
+
+    public static void updateViewSize(SimpleDraweeView draweeView, @Nullable ImageInfo imageInfo) {
+        if (imageInfo != null) {
+            draweeView.getLayoutParams().width = imageInfo.getWidth();
+            draweeView.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            draweeView.setAspectRatio((float) imageInfo.getWidth() / imageInfo.getHeight());
+        }
+    }
+
+    public static void updateViewSizeRatio(SimpleDraweeView draweeView, @Nullable ImageInfo imageInfo) {
+        if (imageInfo != null) {
+            float ratio = (float) imageInfo.getWidth() / imageInfo.getHeight();
+            //draweeView.getLayoutParams().width = imageInfo.getWidth();
+            draweeView.getLayoutParams().height = (int) (draweeView.getLayoutParams().width / ratio);
+            draweeView.setAspectRatio(ratio);
+        }
+    }
+
+
 
 
 
@@ -311,9 +375,9 @@ public abstract class BaseSliderView {
      *
      * @return The current user-provided Picasso instance, or null if none
      */
-    public Picasso getPicasso() {
-        return mPicasso;
-    }
+//    public Picasso getPicasso() {
+//        return mPicasso;
+//    }
 
     /**
      * Provide a Picasso instance to use when loading pictures, this is useful if you have a
@@ -322,7 +386,7 @@ public abstract class BaseSliderView {
      * @param picasso The Picasso instance to use, may be null to let the system use the default
      *                instance
      */
-    public void setPicasso(Picasso picasso) {
-        mPicasso = picasso;
-    }
+//    public void setPicasso(Picasso picasso) {
+//        mPicasso = picasso;
+//    }
 }
